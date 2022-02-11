741374a4c73418e80a9174ddc3648daf49c5a4c3
Tabor Wondimu <csmajortabor@gmail.com>
2022-02-10 16:56:57 -0800

    Finished Excersie

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace SelectionStatements
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Guess My Favourite Number!!!!");
+            var userInput = int.Parse(Console.ReadLine());
+
+            var favNumber = 200;
+            if (userInput == favNumber)
+            {
+                Console.WriteLine("You have guessed right");
+            }
+            else if (userInput < favNumber)
+            {
+                Console.WriteLine("Number is too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Number is too high");
+
+            }
+            else
+            {
+                Console.WriteLine("NeverMind");
+            }    
+        }
+    }
+}
